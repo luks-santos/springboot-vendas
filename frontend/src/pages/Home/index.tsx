bff79c53bd60dc56c0af226482de7ac8651a7ba1
Lucas Batista <lucas.bt46@gmail.com>
2023-10-16 16:01:03 -0300

    edição

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -8,10 +8,10 @@ const Home = () => {
             <NavBar />
             <div className="container">
                 <div className="jumbotron">
-                    <h1 className="display-4">DSVendas</h1>
+                    <h1 className="display-4">Desempenho de Vendas</h1>
                     <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
                     <hr />
-                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um backend construído com Spring Boot.</p>
                     <Link className="btn btn-primary btn-lg" to="/dashboard">
                         Acessar dashboard
                     </Link>
